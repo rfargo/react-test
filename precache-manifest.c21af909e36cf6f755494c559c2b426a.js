dbd84fce628dab5adbeb89dcf257d692df2ec3b8
rfargo <renaldy_fargo@yahoo.com>
2020-07-30 11:19:16 +0800

    Updates

---
+++ precache-manifest.c21af909e36cf6f755494c559c2b426a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884d5f7bd545cf5713132e418c6ef774",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "269008a2489870f11860",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269008a2489870f11860",
+    "url": "/react-test/static/js/main.edd8de09.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
